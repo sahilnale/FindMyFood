e327b1343f460706bedd2512c414a4e30d6b1ce6
Sahil <sahilnale@icloud.com>
2024-06-24 19:53:51 -0700

    Initial Commit

---
+++ FindMyFood/FindMyFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyFoodApp.swift
+//  FindMyFood
+//
+//  Created by Sahil  on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FindMyFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
