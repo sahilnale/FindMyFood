862d396a2cb0eff1e17729e742bdc611d47d17b6
Sahil <sahilnale@icloud.com>
2024-06-25 10:41:31 -0700

    right before user auth

---
+++ FindMyFood/AuthViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  AuthViewModel.swift
+//  FindMyFood
+//
+//  Created by Sahil  on 6/25/24.
+//
+
+import Foundation
+import Firebase
+
+class AuthViewModel: ObservableObject{
+    @Published var userSession:FirebaseAuth.User?
+    @Published var currentUser: User?
+    
+    init(){
+        
+    }
+    
+    func signIn(withEmail email: String, password: String) async throws{
+        
+    }
+    
+    func createUser(withEmail email: String, password: String, fullname: String) async throws{
+        
+    }
+    
+    func signOut(){
+        
+    }
+    
+    func fetchUser() async{
+        
+    }
+}
